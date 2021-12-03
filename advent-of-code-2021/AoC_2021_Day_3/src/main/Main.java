1ae22407e072cb61a5691e807f0c13fcca0d1a08
devzh521 <devzh521@github.com>
2021-12-04 00:17:47 +0100

    aoc2021d3-1 setup

---
+++ advent-of-code-2021/AoC_2021_Day_3/src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+/**
+ * Main class to solve advent of code day 3 2021
+ * @author Hoa Truong
+ *
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        String EXAMPLE_FILE_PATH = "data/aoc_2021_day_3_data_example.txt";
+        String TASK_FILE_PATH = "data/aoc_2021_day_3_data_task.txt";
+
+        System.out.println("Advent of code 2021 day 3-1 solved: ");
+        System.exit(0);
+    }
+}
