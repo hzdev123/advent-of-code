7b303736d98221ea5362ddcae031d1635733a888
devzh521 <devzh521@github.com>
2021-12-05 23:38:52 +0100

    aoc2021d5-1 solved with tests

---
+++ advent-of-code-2021/AoC_2021_Day_5/src/test/TestAoC2021D5.java
@@ -0,0 +1,42 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import main.PointCounter;
+
+public class TestAoC2021D5 {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Testing advent of code 2021 Day 5");
+        String EXAMPLE_FILE_PATH = "data/aoc_2021_day_5_data_example.txt";
+        String TASK_FILE_PATH = "data/aoc_2021_day_5_data_task.txt";
+
+        testDay51Example(EXAMPLE_FILE_PATH);
+        testDay51Task(TASK_FILE_PATH);
+        testDay52Example(EXAMPLE_FILE_PATH);
+        testDay52Task(TASK_FILE_PATH);
+
+        System.out.println("Testing advent of code 2021 Day 5 done");
+    }
+
+    @Test
+    private static void testDay51Example(String filePath) {
+        assertEquals(5, PointCounter.count(filePath, true));
+    }
+
+    @Test
+    private static void testDay51Task(String filePath) {
+        assertEquals(5294, PointCounter.count(filePath, true));
+    }
+
+    @Test
+    private static void testDay52Example(String filePath) {
+        assertEquals(5, PointCounter.count(filePath, false));
+    }
+
+    @Test
+    private static void testDay52Task(String filePath) {
+        assertEquals(5, PointCounter.count(filePath, false));
+    }
+}
