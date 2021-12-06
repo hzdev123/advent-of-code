69dcd91307802c2a50036168422766e3c315bc9d
devzh521 <devzh521@github.com>
2021-12-06 12:53:03 +0100

    aoc2021d5-1 solved with tests

---
+++ advent-of-code-2021/AoC_2021_Day_6/src/main/FishCounter.java
@@ -0,0 +1,65 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import java.lang.StringBuilder;
+
+/**
+ * Represents a fish counter
+ * @author Hoa Truong
+ *
+ */
+public class FishCounter {
+
+    /**
+     * Counts the points along the lines
+     * @param filePath Path to input data file.
+     * @param days  number of simulation days 
+     * @return int number of fishes at the input day 
+     */
+    public static int count(String filePath, int days) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line = "";
+            ArrayList<Integer> fishes = null;
+            while ((line = br.readLine()) != null) {
+                System.out.println("LINE: " + line);
+                fishes = loadFishes(line);
+            }
+            for (int i = 0; i < days; i++) {
+                System.out.println("Day[" + i + "]: " + Arrays.toString(fishes.toArray()));
+                for (int fishIdx = 0; fishIdx < fishes.size(); fishIdx++) {
+                    int fishTimer = fishes.get(fishIdx);
+                    if (fishTimer == 0) {
+                        fishes.set(fishIdx, 6);
+                        fishes.add(9);
+                    } else {
+                        fishes.set(fishIdx, fishTimer - 1);
+                    }
+                }
+            }
+            return fishes.size();
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + filePath);
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    private static ArrayList<Integer> loadFishes(String line) {
+        ArrayList<Integer> fishes = new ArrayList<Integer>();
+        String[] fishTimers = line.split(",");
+        for (int i = 0; i < fishTimers.length; i++) {
+            fishes.add(Integer.parseInt(fishTimers[i]));
+        }
+        return fishes;
+    }
+}
---
+++ advent-of-code-2021/AoC_2021_Day_6/src/test/TestAoC2021D6.java
@@ -0,0 +1,31 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import main.FishCounter;
+
+public class TestAoC2021D6 {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Testing advent of code 2021 Day 6");
+        String EXAMPLE_FILE_PATH = "data/aoc_2021_day_6_data_example.txt";
+        String TASK_FILE_PATH = "data/aoc_2021_day_6_data_task.txt";
+
+        testDay61Example(EXAMPLE_FILE_PATH);
+        testDay61Task(TASK_FILE_PATH);
+
+        System.out.println("Testing advent of code 2021 Day 6 done");
+    }
+
+    @Test
+    private static void testDay61Example(String filePath) {
+        assertEquals(5934, FishCounter.count(filePath, 80));
+    }
+
+    @Test
+    private static void testDay61Task(String filePath) {
+        assertEquals(352195, FishCounter.count(filePath, 80));
+    }
+
+}
