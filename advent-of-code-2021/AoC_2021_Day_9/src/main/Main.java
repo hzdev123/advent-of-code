375462b9851230ce81196ebc21c680ff8f88952e
devzh521 <devzh521@github.com>
2021-12-09 00:38:07 +0100

    aoc2021d9 setup

---
+++ advent-of-code-2021/AoC_2021_Day_9/src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+/**
+ * Main class to solve advent of code day 9 2021
+ * @author Hoa Truong
+ *
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        String EXAMPLE_FILE_PATH = "data/aoc_2021_day_9_data_example.txt";
+        String TASK_FILE_PATH = "data/aoc_2021_day_9_data_task.txt";
+
+        System.out.println("Advent of code 2021 day 9-1 solved: "
+            + NumberCounter.countUnique(TASK_FILE_PATH));
+        System.out.println("Advent of code 2021 day 9-2 solved: "
+            + NumberCounter.countOutputSum(TASK_FILE_PATH));
+        System.exit(0);
+    }
+}
---
+++ advent-of-code-2021/AoC_2021_Day_9/src/test/TestAoC2021D9.java
@@ -0,0 +1,42 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import main.NumberCounter;
+
+public class TestAoC2021D9 {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Testing advent of code 2021 Day 9");
+        String EXAMPLE_FILE_PATH = "data/aoc_2021_day_9_data_example.txt";
+        String TASK_FILE_PATH = "data/aoc_2021_day_9_data_task.txt";
+
+        testDay91Example(EXAMPLE_FILE_PATH);
+        testDay91Task(TASK_FILE_PATH);
+        testDay92Example(EXAMPLE_FILE_PATH);
+        testDay92Task(TASK_FILE_PATH);
+
+        System.out.println("Testing advent of code 2021 Day 9 done");
+    }
+
+    @Test
+    private static void testDay91Example(String filePath) {
+        assertEquals(-1, NumberCounter.countUnique(filePath));
+    }
+
+    @Test
+    private static void testDay91Task(String filePath) {
+        assertEquals(-1, NumberCounter.countUnique(filePath));
+    }
+
+    @Test
+    private static void testDay92Example(String filePath) {
+        assertEquals(-1, NumberCounter.countOutputSum(filePath));
+    }
+
+    @Test
+    private static void testDay92Task(String filePath) {
+        assertEquals(-1, NumberCounter.countOutputSum(filePath));
+    }
+}
