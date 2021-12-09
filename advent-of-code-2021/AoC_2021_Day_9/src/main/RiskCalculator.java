e4f8956dd9224127d5f29a5c9cca957f9a393c0b
devzh521 <devzh521@github.com>
2021-12-09 13:14:10 +0100

    aoc2021d9-1 above, below, right, left added

--- advent-of-code-2021/AoC_2021_Day_9/src/main/RiskCalculator.java
+++ advent-of-code-2021/AoC_2021_Day_9/src/main/RiskCalculator.java
@@ -33,24 +33,25 @@ public class RiskCalculator {
             int mapYlength = 0;
             ArrayList<Integer> digits = new ArrayList<Integer>();
             while ((line = br.readLine()) != null) {
-                System.out.println("LINE: " + line);
+//                System.out.println("LINE: " + line);
                 String[] digit = line.split("");
                 if (mapXlength  == 0) {
-                	mapXlength  = digit.length;
+                    mapXlength  = digit.length;
                 }
                 for (int digitIdx = 0; digitIdx < digit.length; digitIdx++) {
-                	digits.add(Integer.parseInt(digit[digitIdx]));
+                    digits.add(Integer.parseInt(digit[digitIdx]));
                 }
                 mapYlength++;
             }
-            System.out.println("LINE2: " + Arrays.toString(digits.toArray()));
-            System.out.println("mapXlength: " + mapXlength);
-            System.out.println("mapYlength: " + mapYlength);
+//            System.out.println("LINE2: " + Arrays.toString(digits.toArray()));
+//            System.out.println("mapXlength: " + mapXlength);
+//            System.out.println("mapYlength: " + mapYlength + "\n");
 
             //Process the indata
             for (int listIdx = 0; listIdx < digits.size(); listIdx++) {
                 int currentDigit = digits.get(listIdx);
-                if (isLowestPoint(digits, currentDigit, mapXlength, mapYlength)) {
+                if (isLowPoint(digits, currentDigit, listIdx, mapXlength, mapYlength)) {
+                    System.out.println("lowPoint[" + listIdx+ "]: " + currentDigit + "\n");
                     riskLevelSum += currentDigit + 1;
                 }
             }
@@ -64,12 +65,53 @@ public class RiskCalculator {
         return -1;
     }
 
-    private static boolean isLowestPoint(ArrayList<Integer> digits, int currentDigit, int mapXlength, int mapYlength) {
-        int above = 0;
-        int below = 0;
-        int left = 0;
-        int right = 0;
+    private static int getAboveDigit(ArrayList<Integer> digits, int listIdx, int mapXlength) {
+        if (listIdx > mapXlength - 1) {
+//        	  System.out.println("listIdx: " + listIdx);
+//            System.out.println("  Above digit: " + digits.get(listIdx - mapXlength));
+            return digits.get(listIdx - mapXlength);
+        } else {
+            return Integer.MAX_VALUE;
+        }
+    }
 
+    private static int getBelowDigit(ArrayList<Integer> digits, int listIdx, int mapXlength) {
+        if (listIdx < digits.size() - mapXlength) {
+//            System.out.println("listIdx: " + listIdx);
+//            System.out.println("  Below digit: " + digits.get(listIdx + mapXlength));
+            return digits.get(listIdx + mapXlength);
+        } else {
+            return Integer.MAX_VALUE;
+        }
+    }
+
+    private static int getLeftDigit(ArrayList<Integer> digits, int listIdx, int mapXlength) {
+        if (listIdx > 0
+            && listIdx % mapXlength != 0) {
+//            System.out.println("listIdx: " + listIdx);
+//            System.out.println("  Left digit: " + digits.get(listIdx -1));
+            return digits.get(listIdx - 1);
+        } else {
+            return Integer.MAX_VALUE;
+        }
+    }
+
+    private static int getRightDigit(ArrayList<Integer> digits, int listIdx, int mapXlength) {
+        if (listIdx < digits.size() - 1
+            && listIdx % mapXlength != 9) {
+//            System.out.println("listIdx: " + listIdx);
+//            System.out.println("  Right digit: " + digits.get(listIdx + 1));
+            return digits.get(listIdx + 1);
+        } else {
+            return Integer.MAX_VALUE;
+        }
+    }
+
+    private static boolean isLowPoint(ArrayList<Integer> digits, int currentDigit, int listIdx, int mapXlength, int mapYlength) {
+        int above = getAboveDigit(digits, listIdx, mapXlength);
+        int below = getBelowDigit(digits, listIdx, mapXlength);
+        int left = getLeftDigit(digits, listIdx, mapXlength);
+        int right = getRightDigit(digits, listIdx, mapXlength);
         return currentDigit < above
             && currentDigit < below
             && currentDigit < left
