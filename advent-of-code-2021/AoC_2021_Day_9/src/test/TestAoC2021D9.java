3763e9e1b149e194ccb8160a378a54251a7189e5
devzh521 <devzh521@github.com>
2021-12-09 13:32:17 +0100

    aoc2021d9-2 setup

--- advent-of-code-2021/AoC_2021_Day_9/src/test/TestAoC2021D9.java
+++ advent-of-code-2021/AoC_2021_Day_9/src/test/TestAoC2021D9.java
@@ -32,7 +32,7 @@ public class TestAoC2021D9 {
 
     @Test
     private static void testDay92Example(String filePath) {
-        assertEquals(-1, RiskCalculator.getSum(filePath));
+        assertEquals(1134, RiskCalculator.getSum(filePath));
     }
 
     @Test
