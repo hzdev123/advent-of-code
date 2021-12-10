1e78c1d4dab6335d4c835b9bde204d88a848bff6
devzh521 <devzh521@github.com>
2021-12-11 00:53:14 +0100

    aoc2021d10-2 solved with tests

--- advent-of-code-2021/AoC_2021_Day_10/src/main/SyntaxChecker.java
+++ advent-of-code-2021/AoC_2021_Day_10/src/main/SyntaxChecker.java
@@ -8,14 +8,14 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Stack;
-import java.util.TreeSet;
+
+import java.math.BigInteger;
 
 /**
  * Represents a syntax checker
@@ -29,8 +29,10 @@ public class SyntaxChecker {
     private Map<String, Integer> errorMap= null;
     private Map<String, String> getOpen = null;
     private List<String> incompleteLines = null;
-    private Map<String, Integer> incompleteMap = null;
-    private List<Integer> incompleteScores = null;
+    private Map<String, BigInteger> incompleteMap = null;
+    private Stack<String> lineStack = null;
+    private Stack<String> closingStack = null;
+    private Map<String, String> getClosing = null;
 
     public SyntaxChecker() {
         stack = new Stack<String>();
@@ -59,14 +61,20 @@ public class SyntaxChecker {
         errorMap.put(")", 3);
 //        System.out.println("errorScore: " + Arrays.toString(errorScore.entrySet().toArray()));
         incompleteLines = new ArrayList<String>();
-        incompleteMap= new HashMap<String, Integer>();
-        incompleteMap.put(">", 4);
-        incompleteMap.put("}", 3);
-        incompleteMap.put("]", 2);
-        incompleteMap.put(")", 1);
-//        System.out.println("incompleteScore: " + Arrays.toString(incompleteScore.entrySet().toArray()));
-        incompleteScores = new ArrayList<Integer>();
-
+        incompleteMap= new HashMap<String, BigInteger>();
+        incompleteMap.put(">", BigInteger.valueOf(4));
+        incompleteMap.put("}", BigInteger.valueOf(3));
+        incompleteMap.put("]", BigInteger.valueOf(2));
+        incompleteMap.put(")", BigInteger.valueOf(1));
+//        System.out.println("incompleteMap: " + Arrays.toString(incompleteMap.entrySet().toArray()));
+        lineStack = new Stack<String>();
+        closingStack = new Stack<String>();
+        getClosing = new HashMap<String, String>();
+        getClosing.put("<", ">");
+        getClosing.put("{", "}");
+        getClosing.put("[", "]");
+        getClosing.put("(", ")");
+//        System.out.println("getClosing: " + Arrays.toString(getClosing.entrySet().toArray()));
 
 }
 
@@ -75,28 +83,61 @@ public class SyntaxChecker {
      * @param filePath Path to input data file.
      * @return int the incomplete lines middle score
      */
-    public int getIncompletesMiddleScore(String filePath) {
+    public BigInteger getIncompletesMiddleScore(String filePath) {
 //      System.out.println("incomplete lines: " + Arrays.toString(incompleteLines.toArray()));
+        List<BigInteger> incompleteScores = new ArrayList<BigInteger>();
         for (int i = 0; i < incompleteLines.size(); i++) {
 //            System.out.println("incomplete line: " + incompleteLines.get(i));
             incompleteScores.add(
-                getIncompleteScore(incompleteLines.get(i)));
+                getIncompleteLineScore(incompleteLines.get(i)));
         }
-        System.out.println("incompleteScores: " + Arrays.toString(incompleteScores.toArray()));
-        return incompleteScores.get(incompleteScores.size()/2 + 1);
+//        System.out.println("incompleteScores: " + Arrays.toString(incompleteScores.toArray()));
+        Collections.sort(incompleteScores);
+        return incompleteScores.get(incompleteScores.size()/2);
     }
 
-    private int getIncompleteScore(String line) {
-        System.out.println("getIncompleteScore line: " + line);
-        int incompleteScore = 0;
-        String[] chars = line.split("");
-        for (int charIdx = 0; charIdx < chars.length; charIdx++) {
-            String currChar = chars[charIdx];
-
+    private BigInteger getIncompleteLineScore(String line) {
+//        System.out.println("getIncompleteScore line: " + line);
+        BigInteger incompleteScore = BigInteger.valueOf(0);
+        fillLineStack(line);
+        while (lineStack.size() > 0) {
+            String currChar = lineStack.pop();
+            if (closeSet.contains(currChar)) {
+                closingStack.push(currChar);
+            } else {
+                if (closingStack.size() > 0) {
+                    String correctClosing = getClosing.get(currChar);
+                    String topClosing = closingStack.pop();
+                    if (!correctClosing.equals(topClosing)) {
+//                        System.out.println("non matching score: " + currChar);
+                        incompleteScore = updateIncompleteScore(currChar, incompleteScore);
+                    }
+                } else {
+//                    System.out.println("empty close stack score: " + currChar);
+                    incompleteScore = updateIncompleteScore(currChar, incompleteScore);
+                }
             }
         }
-//            incompleteScore += 5 * incompleteScore + incompleteMap.get(")");
+        lineStack.clear();
+        closingStack.clear();
+//        System.out.println("getIncompleteScore line: " + line + " -> " + incompleteScore);
         return incompleteScore;
+    }
+
+    private BigInteger updateIncompleteScore(String currChar, BigInteger currentIncompleteScore) {
+//        System.out.println("updateIncompleteScore start: " + currChar + " -> " + currentIncompleteScore);
+        String correctClosing = getClosing.get(currChar);
+        BigInteger product = BigInteger.valueOf(5)
+            .multiply(currentIncompleteScore);
+        return product.add(incompleteMap.get(correctClosing));
+    }
+
+    private void fillLineStack(String line) {
+        String[] chars = line.split("");
+        for (int charIdx = 0; charIdx < chars.length; charIdx++) {
+            lineStack.push(chars[charIdx]);
+        }
+//        System.out.println("filled lineStack: " + Arrays.toString(lineStack.toArray()));
     }
 
     /**
@@ -146,7 +187,7 @@ public class SyntaxChecker {
         }
 //        System.out.println("incomplete line: " + line);
         incompleteLines.add(line);
-        stack.setSize(0);
+        stack.clear();
         return 0;
     }
 }
