a393518f273b34116ab0ba14231348369e5a8154
devzh521 <devzh521@github.com>
2021-12-13 10:39:52 +0100

    aoc2021d13-1 setup

--- advent-of-code-2021/AoC_2021_Day_13/src/main/Main.java
+++ advent-of-code-2021/AoC_2021_Day_13/src/main/Main.java
@@ -11,10 +11,12 @@ public class Main {
         String EXAMPLE_FILE_PATH = "data/aoc_2021_day_13_data_example.txt";
         String TASK_FILE_PATH = "data/aoc_2021_day_13_data_task.txt";
 
+        DotCounter dc = new DotCounter();
+
         System.out.println("Advent of code 2021 day 13-1 solved: "
-            + NumberCounter.countUnique(TASK_FILE_PATH));
-        System.out.println("Advent of code 2021 day 13-2 solved: "
-            + NumberCounter.countOutputSum(TASK_FILE_PATH));
+            + dc.countDots(TASK_FILE_PATH));
+//        System.out.println("Advent of code 2021 day 13-2 solved: "
+//            + dc.countDots(TASK_FILE_PATH));
         System.exit(0);
     }
 }
